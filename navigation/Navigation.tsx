82bd25f0cfa6a8fee7b6d3beb2305bfddf5fe464
saifmohamed-dev <155442820+saifmohamed-dev@users.noreply.github.com>
2024-05-01 14:20:26 +0300

    handle protected routes

--- navigation/Navigation.tsx
+++ navigation/Navigation.tsx
@@ -1,6 +1,7 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import LoginScreen from "../screens/LoginScreen";
 import RegisterScreen from "../screens/RegisterScreen";
 import TodoScreen from "../screens/TodoScreen";
@@ -8,12 +9,30 @@ import TodoScreen from "../screens/TodoScreen";
 const Stack = createStackNavigator();
 
 const Navigation = () => {
+  const [userId, setUserId] = useState<string | null>(null);
+
+  useEffect(() => {
+    checkAuthStatus();
+  }, []);
+
+  const checkAuthStatus = async () => {
+    const storedUserId = await AsyncStorage.getItem("userId");
+    if (storedUserId) {
+      setUserId(storedUserId);
+    }
+  };
+
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Register" component={RegisterScreen} />
-        <Stack.Screen name="Todo" component={TodoScreen} />
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        {userId ? (
+          <Stack.Screen name="Todo" component={TodoScreen} />
+        ) : (
+          <>
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="Register" component={RegisterScreen} />
+          </>
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
